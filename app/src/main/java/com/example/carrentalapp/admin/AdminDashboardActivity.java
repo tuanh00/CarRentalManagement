7b452b71c21b7f38ce948958dc2a255101763cab
Tu Anh <june265zz@gmail.com>
2024-10-29 15:41:42 -0400

    Build admindashboard functions: Add New Car and Display All Cars

--- app/src/main/java/com/example/carrentalapp/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/example/carrentalapp/admin/AdminDashboardActivity.java
@@ -1,28 +1,42 @@
 package com.example.carrentalapp.admin;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import androidx.fragment.app.Fragment;
 import com.example.carrentalapp.R;
+import com.example.carrentalapp.common.AddCarFragment;
+import com.example.carrentalapp.common.ViewCarsFragment;
 
-//dev-tuanh: testing to add a comment then push to git.
-//28 oct showing how to pull the code from git repo (tuanh)
+
 public class AdminDashboardActivity extends AppCompatActivity {
+
+    private Button buttonAddCar, buttonViewCars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        buttonAddCar = findViewById(R.id.buttonAddCar);
+        buttonViewCars = findViewById(R.id.buttonViewCars);
+
+        // Set default fragment to AddCarFragment
+        replaceFragment(new AddCarFragment());
+
+        // Set button click listeners to replace fragments
+        buttonAddCar.setOnClickListener(v -> replaceFragment(new AddCarFragment()));
+        buttonViewCars.setOnClickListener(v -> replaceFragment(new ViewCarsFragment()));
     }
+<<<<<<< HEAD
+=======
+
+    // Function to replace fragments
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+    }
+>>>>>>> 3a8824d (Build admindashboard functions: Add New Car and Display All Cars)
 }
